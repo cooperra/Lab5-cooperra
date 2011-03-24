7dff9b0567d987fbcf84c952542112cca136e12a
Luke Kennedy <kennedle@rose-hulman.edu>
2011-03-24 15:59:08 -0400

    Added Second Test

--- Testing.java
+++ Testing.java
@@ -1,17 +1,12 @@
 import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class Testing {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-	
 	@Test
 	public void testFactorOne() {
 		ArrayList<Integer> actual = PrimeFactorFinder.factor(1);
@@ -20,8 +15,11 @@ public class Testing {
 		assertEquals(expected, actual);
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void testFactorTwo() {
+		ArrayList<Integer> actual = PrimeFactorFinder.factor(2);
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(2);
+		assertEquals(expected, actual);
 	}
-
 }
