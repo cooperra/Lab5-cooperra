46cb77b6dbdae63e771365bce695d0242545cd4b
Luke Kennedy <kennedle@rose-hulman.edu>
2011-03-24 16:01:05 -0400

    Forgot Actual Program

---
+++ PrimeFactorFinder.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class PrimeFactorFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static ArrayList<Integer> factor(int n) {
+		return new ArrayList<Integer>();
+	}
+
+}
