09b34e240e870d69f815227c6a57662b672660f0
Luke Kennedy <kennedle@rose-hulman.edu>
2011-03-28 16:38:12 -0400

    Final test passed!

--- PrimeFactorFinder.java
+++ PrimeFactorFinder.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
 
-
 public class PrimeFactorFinder {
-	
+
 	public static ArrayList<Integer> factor(int n) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		while (n % 2 == 0) {
-			primes.add(2);
-			n /= 2;
-		}
-		while (n % 3 == 0) {
-			primes.add(3);
-			n /= 3;
-		}
+		int factor = 2;
+		do {
+			while (n % factor == 0) {
+				primes.add(factor);
+				n /= factor;
+			}
+			factor++;			
+		} while (factor <= n);
 		return primes;
 	}
-
 }
