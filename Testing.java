2ad4c067f761ca867aa265cdf35daa8fa34450e9
Luke Kennedy <kennedle@rose-hulman.edu>
2011-03-28 17:01:33 -0400

    added test for 50 after looking up answers.

--- Testing.java
+++ Testing.java
@@ -123,4 +123,27 @@ public class Testing {
 		expected.add(7);
 		assertEquals(expected, actual);
 	}
+	
+	// Luke
+	@Test
+	public void testGenPrime50() {
+		ArrayList<Integer> actual = PrimeFactorFinder.genPrime(50);
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		expected.add(2);
+		expected.add(3);
+		expected.add(5);
+		expected.add(7);
+		expected.add(11);
+		expected.add(13);
+		expected.add(17);
+		expected.add(19);
+		expected.add(23);
+		expected.add(29);
+		expected.add(31);
+		expected.add(37);
+		expected.add(41);
+		expected.add(43);
+		expected.add(47);
+		assertEquals(expected, actual);
+	}
 }
