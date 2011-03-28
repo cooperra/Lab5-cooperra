f4eece8f928779745c813cef1f6ebe04412fd03f
Luke Kennedy <kennedle@rose-hulman.edu>
2011-03-28 16:15:38 -0400

    Passed Test 3

--- PrimeFactorFinder.java
+++ PrimeFactorFinder.java
@@ -2,17 +2,13 @@ import java.util.ArrayList;
 
 
 public class PrimeFactorFinder {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public static ArrayList<Integer> factor(int n) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
 		if (n % 2 == 0) {
 			primes.add(2);
 		}
+		if (n % 3 == 0) primes.add(3);
 		return primes;
 	}
 
